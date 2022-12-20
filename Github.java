63f31aec6089e82a785c655ca04d280c14eaa722
Hemant <hemant894@gmail.com>
2022-12-20 18:31:17 +0000

    JAVAPROJECTS

---
+++ Github.java
@@ -0,0 +1,25 @@
+package Github;
+
+public class Github {
+    public static void main(String[] args) {System.out.println("GitHub");
+        System.out.println(" What is GITHUB?");
+        System.out.println(" Why use of github for projects?");
+        System.out.println(" Why use of github for projects?");
+        System.out.println(">>> Version Control ");
+        System.out.println(">>> Keep your code in one place.");
+        System.out.println(" >>> Great Collaboration platform.");
+        System.out.println(" What is version Control?");
+        System.out.println(" Version controll is a system that records changes to a file or set of files over time so that you can recall speicific versions later.");
+        System.out.println(" What is Repository?");
+        System.out.println(" a place ,room or container where something is deposited or stored.");
+        System.out.println(" Concept of Git");
+        System.out.println(" Keep track of code history.");
+        System.out.println(" Takes `Snapshots` of your files.");
+        System.out.println(" you decided when to take a snapshot by making a `commit` ");
+        System.out.println(" you can visit any snapshot at any time");
+        System.out.println(" you can stage files before committing.");
+        System.out.println(" Just finish first one");
+        System.out.println(" Try to do more revision.");
+
+    }
+}
